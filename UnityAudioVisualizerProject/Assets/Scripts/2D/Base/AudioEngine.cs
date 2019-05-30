94b03c9798364942aacc4dd65a95dac78c70a9b0
NadinHermann <nadin.hermann@gmail.com>
2019-05-30 21:19:17 +0200

    Added Waveform, Colorsync and Bars

---
+++ UnityAudioVisualizerProject/Assets/Scripts/2D/Base/AudioEngine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEngine : MonoBehaviour
+{
+    public static float spectrumValue { get; private set; }
+    private float[] m_audioSpectrum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_audioSpectrum = new float[128]; //must me power of 2
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.GetSpectrumData(m_audioSpectrum, 0, FFTWindow.Hamming);
+        if (m_audioSpectrum != null && m_audioSpectrum.Length > 0)
+        {
+            spectrumValue = m_audioSpectrum[0] * 100; //maybe needs to be normalized
+        }
+    }
+    
+}
